ee525c42b17dc54da40299818a8deed15504adb4
DannieMuse-Travis <danniemuse6@gmail.com>
2023-05-10 12:27:30 +0100

    Server

---
+++ Server.ts
@@ -0,0 +1,20 @@
+// Study on the Status Codes and give 2 exampple of each of the series
+// endpoint is the route from the backend that gives you access to the
+//server
+// url is the point that allow you to view the frontend 
+// study on http 
+
+
+
+// 
+
+class Stack{
+ stack:number[]
+ constructor(){
+    this.stack=[]
+ }
+ pushStack(value){
+    this.stack.push(value)
+    console.log(this.stack);
+ }
+}
